90e17eb61dc47f58c0d3be554fb3c1578011e7ef
Ian Brault <ian.brault@engineering.ucla.edu>
2019-09-08 20:08:49 -0700

    Implement REPL functionality

--- src/main.c
+++ src/main.c
@@ -12,49 +12,90 @@
 #include <eval.h>
 #include <lex.h>
 
+int eval_expr(token_t* expr, int32_t n_tokens, token_t* result)
+{
+    int rc = 0;
+    // convert infix expression to Reverse Polish (postfix) notation
+    int32_t n_rpn;
+    token_t* rpn = shunting_yard(expr, n_tokens, &n_rpn);
+    if (n_rpn < 0)
+    {
+        rc = n_rpn;
+    }
+    else
+    {
+        // evaluate postfix expression
+        rc = evaluate_rpn(rpn, n_rpn, result);
+        free(rpn);
+    }
+
+    return rc;
+}
+
+void repl()
+{
+    const char* prompt = "\033[1;33m>\033[1;32m>\033[1;34m>\033[0m ";
+    char input[MAX_INPUT_LEN];
+    // main REPL loop
+    while (1)
+    {
+        printf("\n%s", prompt);
+        if (!fgets(input, MAX_INPUT_LEN, stdin))
+        {
+            break;
+        }
+        // lex input string into tokens
+        int32_t n_tokens;
+        token_t* tokens = tokenize(input, &n_tokens);
+        if (n_tokens < 0)
+        {
+            print_err(n_tokens, tokens);
+        }
+
+        // evaluate input
+        token_t result;
+        int rc = eval_expr(tokens, n_tokens, &result);
+        if (rc < 0)
+        {
+            print_err(rc, tokens);
+        }
+        else
+        {
+            printf("%d\n", result.value);
+        }
+        free(tokens);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     if (argc <= 1)
     {
-        eprintf("REPL is not yet supported\n");
-        return EXIT_FAILURE;
+        repl();
     }
-
-    const char* input = argv[1];
-
-    // lex input string into tokens
-    int32_t n_tokens;
-    token_t* tokens = tokenize(input, &n_tokens);
-    if (n_tokens < 0)
+    else
     {
-        print_err(n_tokens, tokens);
-        return EXIT_FAILURE;
-    }
+        // lex input string into tokens
+        int32_t n_tokens;
+        token_t* tokens = tokenize(argv[1], &n_tokens);
+        if (n_tokens < 0)
+        {
+            print_err(n_tokens, tokens);
+            return EXIT_FAILURE;
+        }
 
-    // convert infix expression to Reverse Polish (postfix) notation
-    int32_t n_rpn;
-    token_t* rpn = shunting_yard(tokens, n_tokens, &n_rpn);
-    if (n_rpn < 0)
-    {
-        print_err(n_rpn, tokens);
+        // evaluate input
+        token_t result;
+        int rc = eval_expr(tokens, n_tokens, &result);
+        if (rc < 0)
+        {
+            print_err(rc, tokens);
+            return EXIT_FAILURE;
+        }
+
+        printf("%d\n", result.value);
         free(tokens);
-        return EXIT_FAILURE;
     }
 
-    // evaluate postfix expression
-    token_t res;
-    int rc = evaluate_rpn(rpn, n_rpn, &res);
-    if (rc < 0)
-    {
-        print_err(rc, tokens);
-        free(rpn);
-        free(tokens);
-        return EXIT_FAILURE;
-    }
-    // if evaluation was successful, print result
-    printf("%d\n", res.value);
-
-    free(tokens);
-    free(rpn);
     return EXIT_SUCCESS;
 }
